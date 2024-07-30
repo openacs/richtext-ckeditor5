a76e2582a9f81a85f6d04437b51a5cdffc24c970
Antonio Pisano <antonio@elettrotecnica.it>
2024-07-30 11:41:55 +0000

    Cleanup smelly comments and behavior that would be better implemented in xowiki when we will get rid of the ckeditor4 dependency (post 5.10.1 release)

--- tcl/richtext-init.tcl
+++ tcl/richtext-init.tcl
@@ -1,17 +1,7 @@
 template::util::richtext::register_editor ckeditor5
 
-if {[apm_package_installed_p xowiki]} {
-    #
-    # We become the preferred richtext editor for xowiki, if none was
-    # chosen so far.
-    #
-    set preferred_editor [::parameter::get_global_value \
-                              -package_key xowiki \
-                              -parameter PreferredRichtextEditor]
-    if {$preferred_editor eq ""} {
-        ::parameter::set_global_value \
-            -package_key xowiki \
-            -parameter PreferredRichtextEditor \
-            -value "ckeditor5"
-    }
-}
+# Local variables:
+#    mode: tcl
+#    tcl-indent-level: 4
+#    indent-tabs-mode: nil
+# End:
